7a691a6b814d5a0b182262853fb4d364b2b34f35
Massaer <114248851+MassDAGA@users.noreply.github.com>
2025-04-29 16:43:12 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -21,40 +21,36 @@ import plotly.express as px
 
 st.title('Please Login')
 
-password = st.text_input(“Enter Password”, type=“password”)
+password = st.text_input('Enter Password', type='password')
 
-if st.button(“Submit”):
-   if password == “PASSWORD”:
+if st.button('Submit'):
+   if password == 'PASSWORD':
        st.session_state.authenticated = True
    else:
-       st.error(“Incorrect password.”)
+       st.error('Incorrect password.')
 
 st.stop()
 
-st.title(“MCF Installater Network”)
+st.title('MCF Installater Network')
 
-csv_data = st.secrets[“data”][“csv”]
+csv_data = st.secrets['data']['csv']
 installers = pd.read_csv(io.StringIO(csv_data))
 
 st.dataframe(installers)
 
-st.title(“Installers Location Map”)
-st.sidebar.title(“Filters”)
-
-st.sidebar.header(“Filters”)
+st.title('Installers Location Map')
 
 fig_installers = px.scatter_mapbox(
    installers,
    lat=“Latitude”,
    lon=“Longitude”,
-   hover_name=“Installation Partner”,
-   hover_data=[“Name”],
+   hover_name='Installation Partner',
+   hover_data=['Name'],
    zoom=5,
-   color=“Installation Partner”,
+   color='Installation Partner',
    height=1000,
    width=2000,
 )
-fig_installers.update_layout(mapbox_style=“carto-darkmatter”)
 fig_installers.update_layout(mapbox_style=“carto-darkmatter”, use_container_width=True)
 
 st.plotly_chart(fig_installers)
